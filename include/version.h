ca468d6ba94ec20db874aee78742b3af40a16d56
Ian Fraser <ian@glenborrodale.co.uk>
2021-05-20 21:56:42 +0100

    stop displaying massive negative numbers

--- include/version.h
+++ include/version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.1.66"
+        #define VERSION "0.1.68"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2021-05-20 21:41:30.352884"
+        #define BUILD_TIMESTAMP "2021-05-20 21:54:33.352722"
     #endif
     
