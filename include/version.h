ffa175cfc21ab9342ad3b4b58b6f6897210741b4
Ian Fraser <ian@glenborrodale.co.uk>
2021-05-25 09:36:42 +0100

    Ignore pio.map

--- include/version.h
+++ include/version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.1.69"
+        #define VERSION "0.1.70"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2021-05-20 22:09:16.421453"
+        #define BUILD_TIMESTAMP "2021-05-25 09:34:45.427292"
     #endif
     
