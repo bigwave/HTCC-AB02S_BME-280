aec614756865ed3408be979328595c68151d7b58
Ian Fraser <ian@glenborrodale.co.uk>
2022-10-01 14:41:45 +0100

    Fix voltage displayed during LORA transmit

--- include/version.h
+++ include/version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.1.180"
+        #define VERSION "0.1.192"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2022-10-01 08:48:15.447477"
+        #define BUILD_TIMESTAMP "2022-10-01 14:32:34.279833"
     #endif
     
